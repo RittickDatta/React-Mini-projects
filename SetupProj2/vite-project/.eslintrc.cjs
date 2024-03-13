a0d6de301f95fd8d4b788a50aecd5587aff51760
Rittick Datta <drittick@gmail.com>
2024-03-13 17:06:53 +0100

    React proj with Vite

---
+++ SetupProj2/vite-project/.eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'react/jsx-no-useless-fragment': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ SetupProj2/vite-project/src/App.test.ts
@@ -0,0 +1,2 @@
+import { expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
